449f5c664c761d3e36ff54f9faf64a2a2b9c1a1f
wang <yaquawa@gmail.com>
2017-10-17 11:43:30 +0900

    Add laravel-mix option to prevent webpack from processing css urls.

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,6 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+mix.options({processCssUrls: false})
+    .js('resources/assets/js/app.js', 'public/js')
+    .sass('resources/assets/sass/app.scss', 'public/css');
