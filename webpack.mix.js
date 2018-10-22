5a8351641b20e03d4a84f0d23d6d3bee2a9e1b88
wang <yaquawa@gmail.com>
2018-10-22 17:03:51 +0900

    update

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,5 +12,5 @@ let mix = require('laravel-mix');
  */
 
 mix.options({processCssUrls: false})
-    .js('resources/assets/js/app.js', 'public/js')
-    .sass('resources/assets/sass/app.scss', 'public/css');
+    .js('resources/js/app.js', 'public/js')
+    .sass('resources/sass/app.scss', 'public/css');
